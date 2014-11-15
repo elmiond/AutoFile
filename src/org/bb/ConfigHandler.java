8ba9438805d4303e4943b3f7af2eae863755319c
Ask Bisgaard <Elmiond@gmail.com>
2014-11-15 17:06:55 +0100

    Implemented core service and config handler
    Light testing, seems mostly stable so far

--- src/org/bb/ConfigHandler.java
+++ src/org/bb/ConfigHandler.java
@@ -1,5 +1,132 @@
 package org.bb;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
+
 public class ConfigHandler
 {
+	String configFile = "config.xml";
+	XMLConfiguration config;
+
+	public ConfigHandler()
+	{
+		try
+		{
+			if (Files.notExists(Paths.get(configFile)))
+			{
+				Files.createFile(Paths.get(configFile));
+
+				// TODO tilføj skrivning af en tom basis struktur når når vi ved hvordan
+				// den
+				// skal være
+			}
+			config = new XMLConfiguration(configFile);
+			config.setExpressionEngine(new XPathExpressionEngine());
+
+		} catch (IOException | ConfigurationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public ArrayList<WatchedFolder> getFolders() throws ConfigurationException
+	{
+		ArrayList<WatchedFolder> folders = new ArrayList<WatchedFolder>();
+		
+		LogHandler.out("Reading configuration", 0);
+		
+		String[] ss = config.getStringArray("folders/folder/path");
+		for (String s : ss)
+		{
+			if (Files.exists(Paths.get(s)))
+			{
+				ArrayList<RuleSet> rules = new ArrayList<RuleSet>();
+				String[] mat = config.getStringArray("folders/folder[path = '" + s
+						+ "']/rulesets/ruleset/matchsetname");
+				String[] act = config.getStringArray("folders/folder[path = '" + s
+						+ "']/rulesets/ruleset/actionsetname");
+				int i = 0;
+				boolean run = true;
+				while (run)
+				{
+
+					System.out.println(mat[i]);
+					System.out.println(act[i]);
+					rules.add(new RuleSet(getMatchRule(mat[i]), getActions(act[i])));
+					i++;
+					if (i >= mat.length || i >= act.length)
+					{
+						run = false;
+					}
+				}
+
+				folders.add(new WatchedFolder(Paths.get(s), rules));
+			} else
+			{
+				System.out.format("Could not find folder: %s\n", s);
+			}
+		}
+		return folders;
+	}
+
+	private MatchRule getMatchRule(String name)
+	{
+		String kind = config.getString("matches/match[name = '" + name + "']/kind");
+		System.out.println(kind);
+		switch (kind)
+		{
+		case "regex":
+			String pattern = config.getString("matches/match[name = '" + name
+					+ "']/pattern");
+			return new RegexMatchRule(pattern);
+
+		default:
+			return new RegexMatchRule("");
+		}
+	}
+
+	private ArrayList<Action> getActions(String name)
+	{
+		ArrayList<Action> al = new ArrayList<Action>();
+
+		String[] kinds = config.getStringArray("actionsets/actionset[name = '"
+				+ name + "']/actions/action/kind");
+
+		int i = 1;
+		for (String kind : kinds)
+		{
+			System.out.println(kind);
+			al.add(getAction(name, i, kind));
+			i++;
+		}
+		return al;
+	}
+
+	private Action getAction(String name, int index, String kind)
+	{
+		switch (kind)
+		{
+		case "move":
+			return new MoveAction(Paths.get(config
+					.getString("actionsets/actionset[name = '" + name
+							+ "']/actions/action[" + index + "]/destination")));
+
+		case "copy":
+			return new CopyAction(Paths.get(config
+					.getString("actionsets/actionset[name = '" + name
+							+ "']/actions/action[" + index + "]/destination")));
+
+		default:
+			return new MoveAction(Paths.get(""));
+		}
+	}
 }
--- src/org/bb/Watcher.java
+++ src/org/bb/Watcher.java
@@ -3,6 +3,29 @@
  */
 package org.bb;
 
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.configuration.ConfigurationException;
+
 /**
  * @author Elmiond
  *
@@ -10,11 +33,189 @@ package org.bb;
 public class Watcher implements Runnable
 {
 
-	@Override
-	public void run()
+	private final WatchService watcher;
+	private Map<WatchKey, WatchedFolder> watchedfolders = new HashMap<WatchKey, WatchedFolder>();
+	private final Path config = Paths.get("config.xml");
+	//private ConfigHandler ch = new ConfigHandler();
+
+	@SuppressWarnings("unchecked")
+	static <T> WatchEvent<T> cast(WatchEvent<?> event)
 	{
-		// TODO Auto-generated method stub
-		
+		return (WatchEvent<T>) event;
 	}
 
+	/**
+	 * Register the given directory with the WatchService
+	 */
+	private void register(Path dir, ArrayList<RuleSet> ruleSets) throws IOException
+	{
+		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,
+				ENTRY_MODIFY);
+		if (this.watchedfolders.get(key) == null)
+		{
+			System.out.format("register: %s\n", dir);
+		} else
+		{
+			Path prev = this.watchedfolders.get(key).folderPath;
+			if (!dir.equals(prev))
+			{
+				System.out.format("update: %s -> %s\n", prev, dir);
+			}
+		}
+		this.watchedfolders.put(key, new WatchedFolder(dir, ruleSets));
+	}
+
+	/**
+	 * Register the given directory, and all its sub-directories, with the
+	 * WatchService.
+	 */
+	public void registerAll(final WatchedFolder watchedfolder) throws IOException
+	{
+		// register directory and sub-directories
+		Files.walkFileTree(watchedfolder.folderPath, new SimpleFileVisitor<Path>()
+		{
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir,
+					BasicFileAttributes attrs) throws IOException
+			{
+				register(dir, new ArrayList<RuleSet>(watchedfolder.ruleSets));
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+
+	/**
+	 * Creates a WatchService and registers the given directory
+	 */
+	Watcher() throws IOException
+	{
+		this.watcher = FileSystems.getDefault().newWatchService();
+	}
+
+	/**
+	 * Process all events for keys queued to the watcher
+	 */
+	public void run()
+	{
+		LogHandler.out("====Starting service====", 0);
+		
+		loadConfig();
+		
+		while (true)
+		{
+
+			// wait for key to be signalled
+			WatchKey key;
+			try
+			{
+				key = watcher.take();
+			} catch (InterruptedException x)
+			{
+				return;
+			}
+			WatchedFolder watchedfolder = this.watchedfolders.get(key);
+			if (watchedfolder == null || watchedfolder.folderPath == null)
+			{
+				System.err.println("WatchKey not recognized!!");
+				continue;
+			}
+
+			for (WatchEvent<?> event : key.pollEvents())
+			{
+				WatchEvent.Kind kind = event.kind();
+
+				// TBD - provide example of how OVERFLOW event is handled
+				if (kind == OVERFLOW)
+				{
+					continue;
+				}
+
+				// Context for directory entry event is the file name of entry
+				WatchEvent<Path> ev = cast(event);
+				Path name = ev.context();
+				Path child = watchedfolder.folderPath.resolve(name);
+
+				// print out event
+				System.out.format("%s: %s\n", event.kind().name(), child);
+
+				// if directory is created, and watching recursively, then
+				// register it and its sub-directories
+
+				if (child.toAbsolutePath().equals(config.toAbsolutePath()))
+				{
+					// discard events for AutoFile root dir not pertaining to the config
+					// file
+					if (child.getFileName().toString().equals("config.xml"))
+					{
+						System.out.println("config changed");
+						clearWatchlist();
+						loadConfig();
+					}
+				} else if (!child.getFileName().toString().startsWith("~"))
+				{
+
+					if (kind == ENTRY_CREATE)
+					{
+						try
+						{
+							if (Files.isDirectory(child, NOFOLLOW_LINKS))
+							{
+								registerAll(watchedfolder);
+							} else
+							{
+								watchedfolder.doWork(child);
+							}
+						} catch (IOException x)
+						{
+							// ignore to keep sample readable
+						}
+					}
+					if (kind == ENTRY_MODIFY)
+					{
+						if (!Files.isDirectory(child, NOFOLLOW_LINKS))
+						{
+							watchedfolder.doWork(child);
+						}
+					}
+
+				}
+			}
+
+			// reset key and remove from set if directory no longer accessible
+			if (!key.reset())
+			{
+				this.watchedfolders.remove(key);
+
+				// all directories are inaccessible
+				if (this.watchedfolders.isEmpty())
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	public void clearWatchlist()
+	{
+		this.watchedfolders.clear();
+	}
+
+	public void loadConfig()
+	{
+		ConfigHandler ch = new ConfigHandler();
+		ArrayList<WatchedFolder> watchedfolders;
+		try
+		{
+			register(config.toAbsolutePath().getParent(), new ArrayList<RuleSet>());
+			watchedfolders = ch.getFolders();
+			for (WatchedFolder watchedfolder : watchedfolders)
+			{
+				registerAll(watchedfolder);
+			}
+		} catch (ConfigurationException | IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
