c5cc4be4ffb0f08e7c904254b5381ede38aef02f
Ask Bisgaard <Elmiond@gmail.com>
2014-11-27 11:19:39 +0100

    Added Javadoc to VarHandler

--- src/org/bb/VarHandler.java
+++ src/org/bb/VarHandler.java
@@ -2,9 +2,21 @@ package org.bb;
 
 import java.nio.file.Path;
 
+/**
+ * Package for replacing AutoFile variables with value in strings.
+ * @author      Ask Bisgaard	<Elmiond@gmail.com>
+ * @version     1.0
+ * @since       2014-11-27
+ */
 class VarHandler
 {
 
+	/**
+	 * Checks if file matches definition.
+	 * @param	string	String to replace variables in
+	 * @param	string	Path of file to get values from
+	 * @return 				string with variables replaced with values
+	 */
 	public static String replace(String string, Path filePath)
 	{
 		string = string.replaceAll("AUTOFILE_PATH", filePath.toString().replace("\\", "\\\\"));
